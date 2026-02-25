96c2b158a0e3bb28cea4979ca65f82e7995e9cf8
Andrew Bissell <abissell@gmail.com>
2026-02-25 04:50:59 +0000

    use more general module descriptors

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -15,19 +15,33 @@ dependencies {
 }
 
 extraJavaModuleInfo {
-    module("chronicle-core-2026.3.jar", "chronicle.core") // javautil dep
-    module("posix-2026.2.jar", "net.openhft.posix") // javautil dep
-    module("jnr-a64asm-1.0.0.jar", "jnr.a64asm") // javautil dep
-    module("jnr-x86asm-1.0.2.jar", "jnr.x86asm") // javautil dep
+    module("net.openhft:chronicle-core", "chronicle.core") // javautil dep
+    module("net.openhft:chronicle-analytics", "chronicle.analytics") // javautil transitive dep
+    module("net.openhft:posix", "net.openhft.posix") // javautil dep
+    knownModule("org.slf4j:slf4j-api", "org.slf4j") // javautil transitive dep
+    module("net.java.dev.jna:jna", "com.sun.jna") // javautil transitive dep
+    module("net.java.dev.jna:jna-platform", "com.sun.jna.platform") // javautil transitive dep
+    knownModule("org.jetbrains:annotations", "org.jetbrains.annotations") // javautil transitive dep
+    module("com.github.jnr:jnr-a64asm", "jnr.a64asm") // javautil dep
+    module("com.github.jnr:jnr-x86asm", "jnr.x86asm") // javautil dep
+    module("com.github.jnr:jnr-ffi", "org.jnrproject.ffi") // javautil transitive dep
+    knownModule("com.github.jnr:jffi", "org.jnrproject.jffi") // javautil transitive dep
+    module("com.github.jnr:jnr-constants", "org.jnrproject.constants") // javautil transitive dep
+    knownModule("org.ow2.asm:asm", "org.objectweb.asm") // javautil transitive dep
+    knownModule("org.ow2.asm:asm-commons", "org.objectweb.asm.commons") // javautil transitive dep
+    knownModule("org.ow2.asm:asm-analysis", "org.objectweb.asm.tree.analysis") // javautil transitive dep
+    knownModule("org.ow2.asm:asm-tree", "org.objectweb.asm.tree") // javautil transitive dep
+    knownModule("org.ow2.asm:asm-util", "org.objectweb.asm.util") // javautil transitive dep
+    knownModule("com.google.errorprone:error_prone_annotations", "com.google.errorprone.annotations") // javautil transitive dep
 
     // This does not have to be a complete description (e.g. here 'org.apache.commons.collections' does not export anything here).
     // It only needs to be good enough to work in the context of this application we are building.
-    automaticModule("commons-logging-1.2.jar", "org.apache.commons.logging")
-    module("commons-cli-1.5.0.jar", "org.apache.commons.cli", "1.5.0") {
+    automaticModule("commons-logging:commons-logging", "org.apache.commons.logging")
+    module("commons-cli:commons-cli", "org.apache.commons.cli") {
         exports("org.apache.commons.cli")
     }
-    module("commons-collections-3.2.2.jar", "org.apache.commons.collections", "3.2.2")
-    module("commons-beanutils-1.9.4.jar", "org.apache.commons.beanutils", "1.9.4") {
+    module("commons-collections:commons-collections", "org.apache.commons.collections")
+    module("commons-beanutils:commons-beanutils", "org.apache.commons.beanutils") {
         exports("org.apache.commons.beanutils")
 
         requires("org.apache.commons.logging")
